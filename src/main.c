a68eb2b6d03b94234eba54537a373c503f4a0a49
samu7988 <samu7988@colorado.edu>
2022-04-07 21:13:26 -0600

    pushong another changes

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <syslog.h>
 
-void main()
+int main(char argc, char* argv[])
 {
 	openlog("Hello world", LOG_DEBUG, LOG_DAEMON);
  	printf("sayali helloworld");	
 	syslog(LOG_ERR,"Hello world\n\r");
         closelog();
-   
+   	return 0;
    
 }
