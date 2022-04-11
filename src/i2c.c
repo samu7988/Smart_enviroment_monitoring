199834d811797105897e724fad73785cdcdaa2f6
samu7988 <samu7988@colorado.edu>
2022-04-10 19:11:04 -0600

    Change slave address

--- src/i2c.c
+++ src/i2c.c
@@ -55,6 +55,7 @@ bool i2c_read(uint8_t slave_address, uint8_t reg_addr,uint8_t* buffer,uint8_t nu
         return 1;
     }
 
+    slave_address = (slave_address << 1) | (0x01);
     //to get the device from specific bus(specified from slave addr, and reg_address)
     if (ioctl(fd, I2C_SLAVE, slave_address) < 0) 
     {
@@ -62,7 +63,7 @@ bool i2c_read(uint8_t slave_address, uint8_t reg_addr,uint8_t* buffer,uint8_t nu
         return 1;
     }
 
-    reg_addr = (reg_addr << 1) | (0x01);
+    
     if(write(fd, &reg_addr, 1) < 0) //send reg address as 1st byte
     {
         printf("write failed: %s\n",strerror(errno));
