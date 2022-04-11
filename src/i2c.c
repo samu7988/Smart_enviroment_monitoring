4865237250520722f549dcfad6fe569a769d124e
samu7988 <samu7988@colorado.edu>
2022-04-10 19:55:52 -0600

    test i2c write

--- src/i2c.c
+++ src/i2c.c
@@ -63,7 +63,6 @@ bool i2c_read(uint8_t slave_address, uint8_t reg_addr,uint8_t* buffer,uint8_t nu
         return 1;
     }
 
-    reg_addr = (reg_addr ) | (0x21); 
     if(write(fd, &reg_addr, 1) < 0) //send reg address as 1st byte
     {
         printf("write failed: %s\n",strerror(errno));
--- src/lux_sensor.c
+++ src/lux_sensor.c
@@ -37,12 +37,13 @@
 bool enable_lux_sensor()
 {
     bool status = 0;
-    uint8_t data[2] = {0};
-    status = i2c_read(LUX_SENSOR_ADDRESS,CONFIG_REG,data,2);
+    uint8_t data[2] = {0xF0,0x11};
+    //uint16_t data = 0x11F0
+    // status = i2c_read(LUX_SENSOR_ADDRESS,CONFIG_REG,data,2);
     // printf("\n\r before power on data read: data[0]:%u, data[1]:%u",data[0],data[1]);
 
     // data[0] &= ~(0x01);
-    // status = i2c_write(LUX_SENSOR_ADDRESS,CONFIG_REG,data,2);
+    status = i2c_write(LUX_SENSOR_ADDRESS,CONFIG_REG,data,2);
     
     // status = i2c_read(LUX_SENSOR_ADDRESS,CONFIG_REG,data,2);
     printf("\n\r After power on data read: data[0]:%u, data[1]:%u",data[0],data[1]);
