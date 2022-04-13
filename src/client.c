13b06d13c56cbd748c82e8cee6fd8f3c8dc39f6f
samu7988 <samu7988@colorado.edu>
2022-04-13 00:20:58 -0600

    modified working client.c

--- src/client.c
+++ src/client.c
@@ -4,39 +4,35 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+#define SA struct sockaddr
 int main()
 {
-    printf("Hi I am client");
-    struct sockaddr_in client_address;
+    int clientfd;
+	struct sockaddr_in client_addr;
 
-    //creating a socket
-    int client_fd = socket(AF_INET, SOCK_STREAM,0);
-
-    if(client_fd < 0)
+	// socket create and verification
+	clientfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (clientfd == -1) 
     {
-        printf("\n\rsocket() failed");
-        return -1;
-    }
+		printf("\n\rsocket() failed");
+	}
 
-    client_address.sin_family = AF_INET;
-    client_address.sin_port = htons(7000);
-    client_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+	bzero(&client_addr, sizeof(client_addr));
 
-    int client_status = 0;
-    client_status = connect(client_fd,(struct sockaddr *)&client_address, sizeof(client_address) < 0 );
+	// assign IP, PORT
+	client_addr.sin_family = AF_INET;
+	client_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	client_addr.sin_port = htons(7000);
 
-    if(client_status < 0)
+	// connect the client socket to server socket
+	if (connect(clientfd, (struct sockaddr*)&client_addr, sizeof(client_addr)) != 0) 
     {
-        perror("\n\rConnection failed to establish with server(beaglebone black)");
-        close(client_fd);
-        return -1;
-    }
+		printf("connection with the server failed...\n");
+	}
 
-    printf("\n\r connection successfull");
     char recv_msg[255];
-    recv(client_fd,recv_msg,sizeof(recv_msg),0);
     printf("message received:%s",recv_msg);
-    
+
     while(1)
     {
         printf("\n\rClient connection looks to be established");
