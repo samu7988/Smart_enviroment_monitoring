47d3a866acc81a1b0ef758f62f92248dcb3c3df3
samu7988 <samu7988@colorado.edu>
2022-04-12 20:43:52 -0600

    Adding server and client changes(yet to be tested)

--- src/client.c
+++ src/client.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-void main()
+int main()
 {
     printf("Hi I am client");
+    struct sockaddr_in server_address;
+
+    //creating a socket
+    int client_fd = socket(AF_INET, SOCK_STREAM,0);
+
+    if(client_fd < 0)
+    {
+        printf("\n\rsocket() failed");
+        return -1;
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(7000);
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    int client_status = 0;
+    client_status = connect(client_fd,(struct sockaddr *)&server_address, sizeof(server_address) < 0 );
+
+    if(client_status < 0)
+    {
+        printf("\n\rConnection failed to establish with server(beaglebone black)");
+        close(client_fd);
+        return -1;
+    }
+
+    while(1)
+    {
+        printf("\n\rClient connection looks to be established");
+    }
 }
--- src/server.c
+++ src/server.c
@@ -4,9 +4,9 @@
 /* Title: 
  * Brief: 
  * Author: Sayali Mule
- * Date Created: April 9, 2022
+ * Date Created: April 12, 2022
  * Course: Advanced Embedded Software Development
-
+ 
 
  * Reference[1]: 
  * 
@@ -15,7 +15,11 @@
 /****************************************************************************************
 *					HEADER FILE SECTION
 *****************************************************************************************/
-
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 /**************************************************************************************
 *					GLOBAL VARIABLE
 *******************************************************************************************/
@@ -32,5 +36,45 @@
  *
  * @return 
  *********************************************************************************************/
+bool server_init()
+{
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_fd < 0)
+    {
+        printf("\n\rsocket() server failed");
+        return 1;
+    }
+    struct sockaddr_in  server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(7000);
 
+    int server_status = bind(server_fd, (struct sockaddr*)&server_address, sizeof(server_address));
+    if(server_status)
+    {
+        printf("\n\rbind() failed");
+        return 1;
+    }
+
+    int num_client = 3;
+    server_status = listen(server_fd, num_client);
+    if(server_status == -1)
+    {
+        printf("\n\rlisten() failed");
+        return 1;
+    }
+
+    server_status = accept(server_fd, (struct sockaddr*)0, 0);
+    if(server_status == -1)
+    {
+        printf("\n\raccept() failed");
+        return 1;
+    }
+
+    while(1)
+    {
+        printf("\n\rserver is connected");
+    }
+
+}
 
