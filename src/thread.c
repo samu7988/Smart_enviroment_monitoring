93d7492c30d922e76298e67d6a08b00046591889
samu7988 <samu7988@colorado.edu>
2022-04-14 20:34:02 -0600

    Cleaning up

---
+++ src/thread.c
@@ -0,0 +1,81 @@
+/********************************************************************************************
+*					FILENAME:c
+**********************************************************************************************/
+/* Title: 
+ * Brief: 
+ * Author: Sayali Mule
+ * Date Created: April 9, 2022
+ * Course: Advanced Embedded Software Development
+
+
+ * Reference[1]: 
+ * 
+ * 
+ * */
+/****************************************************************************************
+*					HEADER FILE SECTION
+*****************************************************************************************/
+#include "temperature_sensor.h"
+#include "lux_sensor.h"
+#include <stdio.h>
+#include <stddef.h>
+#include "timer.h"
+/**************************************************************************************
+*					GLOBAL VARIABLE
+*******************************************************************************************/
+
+/**********************************************************************************
+*				FUNCTION DEFINITION
+***************************************************************************************/
+/***********************************************************************************************
+ * @brief 
+ *
+ *
+ *
+ * @param address:
+ *
+ * @return 
+ *********************************************************************************************/
+
+void* temperature_sensor_thread(void* param)
+{
+    bool status = 0;
+    double temperature = 0.0;
+    while(1)
+    {
+        if(temp_sensor_time_expire == 1)
+        {
+            status = read_temperature_value(&temperature,TEMP_CELSIUS);
+
+            if(status)
+            {
+                printf("\n\rread_temperature_value() returned 1");
+            }
+
+            printf("\n\rTemperature value: %lf",temperature);
+            temp_sensor_time_expire = 0;
+        }
+    }
+}
+
+
+void* light_sensor_thread(void* param)
+{
+    bool status = 0;
+    double light_value = 0.0;
+    while(1)
+    {
+        if(light_sensor_time_expire == 1)
+        {
+            status = read_light_value(&light_value);
+
+            if(status)
+            {
+                printf("\n\rread_light_value() returned 1");
+            }
+
+            printf("\n\rlight value: %lf",light_value);
+            light_sensor_time_expire = 0;
+        }
+    }
+}
